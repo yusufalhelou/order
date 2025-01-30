0c1af102497b6d2cc2d6a347ccb1575bf4cf0909
yusufalhelou <108984728+yusufalhelou@users.noreply.github.com>
2025-01-30 05:17:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -56,9 +56,12 @@ const toppingOptions = {
 
 const discountCodes = {
     yusuf: 100,
+    mariam: 99.9,
+    israa: 99.9,
     yomna: 99.9,
     hana: 99.9,
     nada: 99.9,
+    mayar: 00.0,
     fiftyone: 51,
     51: 51,
 };
